f34f78b00f5487bbbe7ef4aaa58d9af38a5e7f80
Brandon Streiff <git@streiff.net>
2012-09-16 10:39:01 -0500

    Add Color and some binary layout tests.

--- common/Color.h
+++ common/Color.h
@@ -13,8 +13,6 @@ public:
    Color();
    Color(uint8_t r, uint8_t g, uint8_t b);
    Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
-   Color(float r, float g, float b);
-   Color(float r, float g, float b, float a);
    explicit Color(const Vector3& rgb);
    explicit Color(const Vector4& rgba);
    Color(const Color& other);
